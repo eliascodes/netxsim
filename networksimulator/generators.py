55782a4165a811dd0da16ed0eff28cfc00f0a647
Elias <elias0789@gmail.com>
2015-11-26 14:50:17 +0000

    Edit generators to be iterables rather than having a get_next method. Add freeze/unfreeze methods

--- networksimulator/generators.py
+++ networksimulator/generators.py
@@ -1,50 +1,70 @@
 from abc import ABCMeta, abstractmethod
-import numpy.random as nprand
+from numpy.random import RandomState
+from networksimulator import agents
 
 
-class AgentGenerator(AbstractGenerator):
+class AgentGenerator(object):
     """"""
     def __init__(self, agent_type):
-        if issubclass(agent_type, AbstractAgent):
+        if issubclass(agent_type, agents.AbstractAgent):
             self.agent_type = agent_type
         else:
             raise TypeError("Input must be an Agent type")
 
-    def get_next(self):
+    def build(self):
+        return iter(self)
+
+    def __iter__(self):
         count = 0
         while True:
             yield self.agent_type(count)
             count += 1
 
 
-class AttributeGenerator(AbstractGenerator):
+class AttributeGenerator(object):
     """"""
     def __init__(self, seed):
-        self.rng = nprand.RandomState(seed)
+        self.rng = RandomState(seed)
         self.attr_deterministic = {}
         self.attr_stochastic = {}
         self._frozen = False
 
     def set_deterministic(self, attr, value):
-        raise BaseException("Frozen") if self._frozen
+        if self._frozen:
+            raise BaseException("Frozen")
         self.attr_deterministic[attr] = value
         return self
 
-    def set_stochastic(self, attr, params):
-        raise BaseException("Frozen") if self._frozen
-        if hasattr(self.rng, params['distribution']):
-            self.attr_stochastic[attr] = params
+    def set_stochastic(self, attr, distribution=None, arguments=None):
+        if self._frozen:
+            raise BaseException("Frozen")
+        elif distribution is not None and arguments is not None:
+            if hasattr(self.rng, distribution):
+                self.attr_stochastic[attr] = {'distribution': distribution, 'arguments': arguments}
+            else:
+                raise AttributeError('{0} is not a valid method of {1}'
+                                     .format(params['distribution'], self.rng))
         else:
-            raise AttributeError('{0} is not a valid method of {1}'
-                                 .format(params['distribution'], self.rng))
+            raise TypeError("Inputs must not be NoneType")
         return self
 
-    def get_next(self):
+    def freeze(self):
         self._frozen = True
+        return self
+
+    def unfreeze(self):
+        self._frozen = False
+        return self
+
+    def build(self):
+        return iter(self)
+
+    def __iter__(self):
+        self.freeze()
         while True:
             attr_dic = self.attr_deterministic
             for key in self.attr_stochastic:
                 distn = self.attr_stochastic[key]['distribution']
                 args = self.attr_stochastic[key]['arguments']
-                attr_dic[attr] = getattr(self.rng, distn)(*args)
+                attr_dic[key] = getattr(self.rng, distn)(*args)
             yield attr_dic
