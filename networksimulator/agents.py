0b45428037b53661388765856e641ce0928faefa
Elias <elias0789@gmail.com>
2015-11-26 22:57:14 +0000

    Simplify; remove abstract agent, and stochastic and deterministic subclasses, leave only the base agent class.

--- networksimulator/agents.py
+++ networksimulator/agents.py
@@ -1,9 +1,6 @@
-from abc import ABCMeta, abstractmethod
-
-
-class AbstractAgent(object):
+class BaseAgent(object):
     """
-    Abstract base class for all agents.
+    Base class for all agents.
 
     Agents are the active, dynamic components of a network simulation. In any simulation, there is a
     one-to-one correspondence between agents and nodes.
@@ -12,8 +9,6 @@ class AbstractAgent(object):
     At this level of abstraction, we make no restriction on the states that the agent has access to
     both read and write.
     """
-    __metaclass__ = ABCMeta
-
     def __init__(self, agent_id):
         self.agent_id = agent_id
 
@@ -23,18 +18,5 @@ class AbstractAgent(object):
     def __eq__(self, other):
         return isinstance(other, type(self)) and hash(self) == hash(other)
 
-    @abstractmethod
     def run(self, graph, env):
-        pass
-
-
-class DeterministicAgent(AbstractAgent):
-    """"""
-    def __init__(self, agent_id):
-        super().__init__(agent_id)
-
-
-class StochasticAgent(AbstractAgent):
-    """"""
-    def __init__(self, agent_id):
-        super().__init__(agent_id)
+        yield env.timeout(1)
