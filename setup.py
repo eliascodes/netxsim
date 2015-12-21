9e7f1634a626caf4633ab92f2c57858739dac3eb
Elias <elias0789@gmail.com>
2015-12-21 14:54:58 +0000

    Initial package setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='NetworkSimulator',
+    version='0.0',
+    description='Network/Graph simulation package',
+    author='Elias Malik',
+    packages=['networksimulator']
+)
