eb197173170a29cd93d58f7e9f3567833c363c35
Elias <elias0789@gmail.com>
2015-12-27 18:47:26 +0000

    docstrings

--- networksimulator/environment.py
+++ networksimulator/environment.py
@@ -1,10 +1,29 @@
+""" Simulation environment package
+
+This module defines the base class for the network simulation environment. It inherits from and is
+based on SimPy's Environment class. It defines the context within which the simulation takes place
+and also controls the simulation clock.
+
+An environment is a necessary component of every simulation.
+"""
+
 import simpy
 from numpy.random import RandomState
 
 
 class NetworkEnvironment(simpy.Environment):
-    """"""
+    """Base class defining simulation environment
+
+    Description ...
+    """
     def __init__(self, graph, seed=None, time_start=0):
+        """Constructor
+
+        Args:
+            graph (Object): NetworkX Graph object on which to perform simulation.
+            seed (Optional[int]): Seed for NumPy's RandomState
+            time_start (Optional[int]): Time at which to start simulation
+        """
         super().__init__(initial_time=time_start)
         if seed:
             self.rng = RandomState(seed)
--- networksimulator/simulator.py
+++ networksimulator/simulator.py
@@ -1,39 +1,89 @@
+"""Simulation case and utility functions
+
+"""
+
+
 class NetSimCase(object):
-    """docstring for NetSimCase"""
-    def __init__(self, verbose=False, runtime=0, path_results=''):
-        self.grid = self._prepare_grid()
-        self.verbose = verbose
+    """Base class for simulation case specification
+
+    In order to define a simulation case, this class must be sub-classed, and the _prepare_* methods
+    must be redefined.
+
+    Arguments:
+        runtime : (int) [optional] : The runtime of the simulation
+    """
+
+    timestamp_start = ''
+
+    def __init__(self, runtime=0):
         self.runtime = runtime
-        self.path = path_results
         self.success = False
 
     def run(self):
-        for point in self.grid:
+        """Exectue the simulation
+
+        Runs the simulation for each point in the grid and logs the outputs
+
+        Arguments:
+            None
+        """
+        self.timestamp_start = datetime.datetime.now().strftime('%Y%m%dT%H%M%S')
+
+        for point in self._prepare_grid():
             graph = self._prepare_graph(**point)
             env = self._prepare_env(graph, **point)
             logger = self._prepare_logger(env, **point)
+
             if env.now < self.runtime:
-                env.run(until=self.runtime)
-            results = logger.stop(env).get_results()
-            if self.verbose:
-                results.pprint()
+                try:
+                    env.run(until=self.runtime)
+                    success = True
+                except Exception as e:
+                    success = False
+
+            if success:
+                return (logger, graph)
             else:
-                results.save(self.path)
-        return self.success
+                return success
 
     def _prepare_grid(self):
+        """Creates and returns the parameter grid determining the parameters of each sim case.
+
+        Arguments:
+            None
+        """
         pass
 
     def _prepare_graph(self, **kwargs):
+        """Creates and returns the NetworkX graph object on which each sim case is run.
+
+        Arguments:
+            kwargs : (dict) : key-value pairs determining the grid point parameters
+        """
         pass
 
     def _prepare_env(self, graph, **kwargs):
+        """Creates and returns the SimPy Environment object defining the simulation context.
+
+        Arguments:
+            kwargs : (dict) : key-value pairs determining the grid point parameters
+        """
         pass
 
     def _prepare_logger(self, env, **kwargs):
+        """Creates and returns the NetworkX graph object on which each sim case is run
+
+        Arguments:
+            env : (SimPy.Environment) : Environment object
+            kwargs : (dict) : key-value pairs determining the grid point parameters
+        """
         pass
 
 
 def run_simulation(simcase):
-    """"""
+    """Run simulation
+
+    Arguments:
+        simcase : (NetSimCase) : Simcase object
+    """
     return simcase.run()
