b2a92ac9ccf06c9488c235afdf095aeb6a5626ab
Elias <elias0789@gmail.com>
2015-12-30 18:38:43 +0000

    switch to setuptools to enforce dependencies

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='NetworkSimulator',
     version='0.0',
     description='Network/Graph simulation package',
     author='Elias Malik',
-    packages=['networksimulator']
+    packages=['networksimulator'],
+    install_requires=['networkx']
 )
