adaa2f1d9674c89e7744bfde8827d9f76941dab1
Elias <elias0789@gmail.com>
2016-01-04 18:40:24 +0000

    Delete support sub package

--- networksimulator/tests/support/toyclasses.py
+++
@@ -1,6 +0,0 @@
-from networksimulator import agents
-
-
-class ToyDeterministicAgent(agents.DeterministicAgent):
-    def run(self, graph, env):
-        return None
--- networksimulator/tests/support/utils.py
+++
@@ -1,6 +0,0 @@
-def count_num_samples(samples, mean, sigma_level):
-    count = 0
-    for sample in samples:
-        if sample < mean+sigma_level and sample > mean-sigma_level:
-            count += 1
-    return count
