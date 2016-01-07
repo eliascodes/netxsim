7ead1d7ea9c909d20f623a74ac65cc0fcea28bda
Elias <elias0789@gmail.com>
2016-01-07 20:33:21 +0000

    Tidy up the generator code, add docstrings

--- networksimulator/generators.py
+++ networksimulator/generators.py
@@ -1,17 +1,45 @@
-from numpy.random import RandomState
+import numpy.random as np_rand
+import copy
 from . import agents
 
 
-class AgentGenerator(object):
-    """"""
-    def __init__(self, agent_type):
-        if issubclass(agent_type, agents.BaseAgent):
-            self.agent_type = agent_type
-        else:
-            raise TypeError("Input must be an Agent type")
+class BaseGeneratorBuilder(object):
+    """Base class for generator builders
+    Defines the common interface, and the freezing functionality
+    """
+    def __init__(self):
+        self.__frozen__ = False
+
+    def __iter__(self):
+        raise NotImplementedError
 
     def build(self):
         return iter(self)
+
+    @property
+    def frozen(self):
+        return self.__frozen__
+
+    def freeze(self):
+        self.__frozen__ = True
+
+    def unfreeze(self):
+        self.__frozen__ = False
+
+
+class AgentGeneratorBuilder(BaseGeneratorBuilder):
+    """Builds an Agent generator that yields agents of a given type
+    """
+    def __init__(self, agent_type):
+        """
+        Args:
+            agent_type: <class> defines the agent type to be instantiated and yielded
+        """
+        if not issubclass(agent_type, agents.BaseAgent):
+            raise TypeError("Input must be an Agent type")
+
+        super().__init__()
+        self.agent_type = agent_type
 
     def __iter__(self):
         count = 0
@@ -20,52 +48,55 @@ class AgentGenerator(object):
             count += 1
 
 
-class AttributeGenerator(object):
-    """"""
+class AttributeGeneratorBuilder(BaseGeneratorBuilder):
+    """Builds an node attribute generator that yields attribute dictionaries
+    """
     def __init__(self, seed):
-        if seed is None:
-            seed = 0
-        self.rng = RandomState(seed)
-        self.attr_deterministic = {}
-        self.attr_stochastic = {}
-        self._frozen = False
+        """
+        Args:
+            seed: <int> Seed for numpy's RandomState RNG
+        """
+        super().__init__()
+        self.__rng__ = np_rand.RandomState(seed)
+        self.__constant__ = {}
+        self.__stochastic__ = {}
 
-    def set_deterministic(self, attr, value):  # TODO change to kwargs
-        if self._frozen:
-            raise BaseException("Frozen")
-        self.attr_deterministic[attr] = value
+    def add_constant(self, **kwargs):
+        """Adds an attribute with a constant value for each iteration
+
+        Args:
+            kwargs: <dict> Name-value pairs of constant attributes
+        """
+        if self.frozen:
+            raise BaseException("frozen")
+        for (attr, val) in kwargs.items():
+            self.__constant__[attr] = val
         return self
 
-    def set_stochastic(self, attr, distribution=None, arguments=None):  # TODO change to kwargs
-        if self._frozen:
-            raise BaseException("Frozen")
-        elif distribution is not None and arguments is not None:
-            if hasattr(self.rng, distribution):
-                self.attr_stochastic[attr] = {'distribution': distribution, 'arguments': arguments}
-            else:
-                raise AttributeError('{0} is not a valid method of {1}'.format(distribution, self.rng))
-        else:
-            raise TypeError("Inputs must not be NoneType")
-        return self
+    def add_stochastic(self, attr, **kwargs):
+        """Adds an attribute with a randomly varying value for each iteration
+        Utilises numpy's random number generators and RandomState object
 
-    def freeze(self):
-        self._frozen = True
-        return self
+        Args:
+            attr: <string> Name of the attribute to be generated
+            kwargs: <dict> Distribution-argument pairs, matching methods of numpy's RandomState
 
-    def unfreeze(self):
-        self._frozen = False
+        Example: add_stochastic('hasFlu', binomial=[n, p])
+        """
+        if self.frozen:
+            raise BaseException("frozen")
+        for (k, v) in kwargs.items():
+            if not hasattr(self.__rng__, k):
+                raise AttributeError('{0} is not a valid method of {1}'.format(k, self.__rng__))
+            self.__stochastic__[attr] = {'distribution': k, 'arguments': v}
         return self
-
-    def build(self):
-        return iter(self)
 
     def __iter__(self):
         self.freeze()
         while True:
-            attr_dic = self.attr_deterministic
-            for key in self.attr_stochastic:
-                distn = self.attr_stochastic[key]['distribution']
-                args = self.attr_stochastic[key]['arguments']
-                attr_dic[key] = getattr(self.rng, distn)(*args)
             attr_dic = copy.deepcopy(self.__constant__)
+            for key in self.__stochastic__:
+                dist = self.__stochastic__[key]['distribution']
+                args = self.__stochastic__[key]['arguments']
+                attr_dic[key] = getattr(self.__rng__, dist)(*args)
             yield attr_dic
