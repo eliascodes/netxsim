3eb936f7d532b4d3cefaf044ca4c6922b142a2ac
Elias <elias0789@gmail.com>
2016-02-14 23:49:02 +0000

    Add module and method docstrings

--- networksimulator/agents.py
+++ networksimulator/agents.py
@@ -1,3 +1,28 @@
+"""Agents module
+
+Agents are the primary dynamic component of the network simulation. All nodes are agent objects, and every class of
+agents defines a `run` method, which defines the dynamics of the agent as a generator. This `run` method is registered
+with the simulation environment object as a SimPy process that yields an event.
+
+Agent objects are intended to be stateless. Any node state should be held as a NetworkX node attribute, rather than
+attached as a member of the Agent class. The `run` method has access to the full graph and simulation environment via
+arguments, and should perform any state manipulation using those provided arguments.
+
+For example, an agent which examined its own boolean node attribute called 'state', and inverted it based on a uniformly
+random sample, could be written as follows:
+```
+def run(self, graph, env):
+    while True:
+        if env.draw('uniform') > 0.5:
+            graph.node[self]['state'] = not graph.node[self]['state']
+        yield env.timeout(1)
+```
+Note again that the `run` method is a generator that yields SimPy events. In SimPy terms, this is a process. In this
+case the process yields an arbitrary number of events, but this need not be the case. See the SimPy process
+documentation for more detail.
+"""
+
+
 class BaseAgent(object):
     """
     Base class for all agents.
--- networksimulator/builders.py
+++ networksimulator/builders.py
@@ -1,27 +1,55 @@
+"""Builders module
+
+NetworkX graph objects may be built using the provided graph factories. They make it simple to construct graphs with the
+specified randomly or deterministically distributed attributes or edges.
+
+The user-facing interface is defined by the `[Multi[Di]]GraphFactory` classes, but most of the work is done by the two
+list-builder classes: NodeListBuilder and EdgeListBuilder. These classes build lists of tuples that can be easily used
+to generate a graph using NetworkX's `add_node_from` and `add_edge_from` methods.
+"""
 import itertools
 import networkx as nx
 from numpy import random
 from scipy import stats
-
 from . import agents
 
 
 class BaseListBuilder(object):
-    """Base class for the node and edge list builders used in the GraphFactory class
-
-    """
+    """Base class for the node and edge list builders used in GraphFactory classes"""
     __rng__ = None
     __attr_dic__ = {}
     size = 0
 
     def __init__(self, rng=None):
+        """Constructor
+
+        Args:
+            rng: Instance of NumPy's RandomState object
+
+        Returns:
+            ListBuilder object
+        """
         self.__rng__ = rng
 
     def add(self, **kwargs):
+        """Add attribute name/value pairs to the attribute dictionary
+
+        Args:
+            kwargs: name/value pairs of arguments to parse. Values may
+
+        """
         for name, value in kwargs.items():
             self.__attr_dic__[name] = self._parse_args(value)
 
     def _parse_distribution(self, dist):
+        """Parses a given distribution input into a callable with the given seed
+
+        Args:
+            dist: Callable of scipy.stats, or numpy.random, or a string of either
+
+        Returns:
+            Callable distribution function with the given seeded RNG self.__rng__
+        """
         try:
             dist.random_state = self.__rng__
             dist = dist.rvs
@@ -34,15 +62,42 @@ class BaseListBuilder(object):
         return dist
 
     def _parse_args(self, arg):
+        """Parse the arguments given to .add()
+
+        Args:
+            arg: treated as distribution specification if a tuple
+
+        Returns:
+            If input is a tuple, returns the same 3-tuple with the first element replaced with the appropriate callable.
+            Otherwise, leaves the input unchanged
+        """
         if isinstance(arg, tuple):
             dist = self._parse_distribution(arg[0])
             arg = (dist, arg[1], arg[2]) if len(arg) > 2 else (dist, arg[1], {})
         return arg
 
     def _build_list(self, list_attr, graph):
+        """Specific implementation of the list building method
+        Takes as inputs the list of sampled or constant attributes as well as the full graph
+
+        Args:
+            list_attr: List of dictionaries, where dictionary keys are attributes previously added using .add()
+            graph: Full NetworkX graph object
+
+        Returns:
+            Iterable object
+        """
         raise NotImplementedError
 
     def build(self, graph):
+        """Generic build method, relying on the specific list builder method ._build_list
+
+        Args:
+            graph: Full NetworkX graph object
+
+        Returns:
+            Iterable object
+        """
         samples = {}
         for name, value in self.__attr_dic__.items():
             try:
@@ -56,6 +111,7 @@ class BaseListBuilder(object):
 
 
 class NodeListBuilder(BaseListBuilder):
+    """Defines the specific implementation of the list builder for (node, attributes) tuple list"""
     agent = None
 
     def _build_list(self, list_attr, graph):
@@ -72,6 +128,7 @@ class NodeListBuilder(BaseListBuilder):
 
 
 class EdgeListBuilder(BaseListBuilder):
+    """Defines the specific implementation of the list builder for (edge, attributes) tuple list"""
     __edge_dic__ = {}
     __thd__ = 0
     callback = None
@@ -80,6 +137,12 @@ class EdgeListBuilder(BaseListBuilder):
         super().__init__(rng=rng)
 
     def from_dist(self, spec, thd):
+        """Sets the distribution from which the edge list will be built
+
+        Args:
+            spec: distribution specification tuple. Same form as for add() method
+            thd: threshold above which the edge will be added
+        """
         self.__edge_dic__['edges'] = self._parse_args(spec)
         self.__thd__ = thd
         self.callback = None
@@ -98,10 +161,14 @@ class EdgeListBuilder(BaseListBuilder):
                 if sample > self.__thd__:
                     list_edge.append(pair)
 
-        return [(pair[0], pair[1], dic) for pair, dic in zip(list_edge, list_attr)]
+        list_edge = [(pair[0], pair[1], dic) for pair, dic in zip(list_edge, list_attr)]
+        return list_edge[:self.size] if self.size else list_edge
 
 
 class BaseGraphFactory(object):
+    """Base class for all graph factories.
+    Fully specifies functionality; concrete implementations only specify which NetworkX graph object to instantiate.
+    """
     def __init__(self, rng=None):
         if rng is not None and not isinstance(rng, random.RandomState):
             raise TypeError
@@ -109,6 +176,11 @@ class BaseGraphFactory(object):
         self.__ebuilder__ = EdgeListBuilder(rng)
 
     def build(self):
+        """Build a graph object from the node and edge list builders
+
+        Returns:
+            NetworkX graph object
+        """
         graph = self.init_graph()
 
         # build list of (node, attrdict) node tuples
@@ -125,28 +197,46 @@ class BaseGraphFactory(object):
         raise NotImplementedError
 
     def set_size(self, number_of_nodes):
+        """Specify the number of nodes in the graph to be built"""
         self.__nbuilder__.size = number_of_nodes
 
     def set_edge_limit(self, limit_num_edges):
+        """Specify an upper limit on the number of edges in the graph"""
         self.__ebuilder__.size = limit_num_edges
 
     def set_agent(self, agent_type):
+        """Specify the agent class to be used to populate the graph nodes"""
         if not issubclass(agent_type, agents.BaseAgent):
             raise TypeError
         self.__nbuilder__.agent = agent_type
 
     def set_node_attribute(self, **kwargs):
+        """Set node attributes as keyword arguments.
+        Tuples are interpreted as random distribution specifications, with the form (dist, args, kwargs), where dist is
+        a method of numpy.random or scipy.stats, or a string of the name of either. Non-tuples are left unmodified.
+        """
         self.__nbuilder__.add(**kwargs)
 
     def set_edge_attribute(self, **kwargs):
+        """Set edge attributes as keyword arguments.
+        Arguments are interpreted in the same way as in .set_node_attribute
+        """
         self.__ebuilder__.add(**kwargs)
 
     def set_edge_by_distribution(self, arg_tuple, threshold):
+        """Set the distribution according to which edges should be added.
+        The arg_tuple must be a distribution specification (dist, args, kwargs) and the threshold is the value above
+        which the edge is added for a given random sample
+        """
         if not isinstance(arg_tuple, tuple):
             raise TypeError
         self.__ebuilder__.from_dist(arg_tuple, threshold)
 
     def set_edge_by_callback(self, cb):
+        """Set the edges based on the given callable.
+        The callable must have the signature (nodeA, nodeB, graph, rng) and return a boolean indicating whether or not
+        the edge between nodeA and nodeB should be added to the graph
+        """
         if not callable(cb):
             raise TypeError
         self.__ebuilder__.callback = cb
